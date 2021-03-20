f73f769e19d9c8e854a2c05a40f0e1910ac15042
nastyadunaeva <56322861+nastyadunaeva@users.noreply.github.com>
2021-03-21 00:00:55 +0300

    Add files via upload

---
+++ ClassLibrary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        public static void data_change(object sourse, DataChangedEventArgs args)
+        {
+            Console.WriteLine();
+            Console.WriteLine(args.ToString());
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
